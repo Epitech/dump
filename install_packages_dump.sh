e50a0610057c107349f0393cbdd1d1c99d844b68
Jérémy Levilain <jeremy.levilain@epitech.eu>
2019-03-19 19:48:11 +0100

    feat: add libcaca and its header

--- install_packages_dump.sh
+++ install_packages_dump.sh
@@ -37,6 +37,8 @@ packages_list=(boost-devel.x86_64
 	       SDL2_ttf-devel.x86_64
 	       SDL2_image.x86_64
 	       SDL2_image-devel.x86_64
+	       libcaca.x86-64
+	       libcaca-devel.x86_64
 	       libX11-devel.x86_64
 	       libXext-devel.x86_64
 	       ltrace.x86_64
