aedce80f725fa65864ca82a938505f18cca8a710
Alexandre Vanhecke <alexandre1.vanhecke@epitech.eu>
2020-07-22 08:41:31 +0200

    feat: Update epitech-emacs

--- install_packages_dump.sh
+++ install_packages_dump.sh
@@ -135,7 +135,7 @@ cp blih.py /usr/bin/blih
 # CONFIG EMACS
 git clone https://github.com/Epitech/epitech-emacs.git
 cd epitech-emacs
-git checkout 1f1ef05d25058e20050612e71578312bee513a5b
+git checkout 278bb6a630e6474f99028a8ee1a5c763e943d9a3
 ./INSTALL.sh system
 cd .. && rm -rf epitech-emacs
 
