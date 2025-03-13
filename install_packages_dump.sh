49af81cde035a5cc2d810d1e58ea06003cc0b1d8
Extauren <extauren@protonmail.com>
2025-03-13 15:42:42 +0100

    fix: remove docker offical repository and install docker compose throw ubuntu repository

--- install_packages_dump.sh
+++ install_packages_dump.sh
@@ -32,16 +32,6 @@ add-apt-repository -y -s universe
 apt update
 apt upgrade -y
 
-apt-get install -y ca-certificates curl
-apt-get install -y -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-
 echo "wireshark-common wireshark-common/install-setuid boolean true" | debconf-set-selections
 
 packages_list=(
@@ -55,11 +45,8 @@ packages_list=(
     cmake
     curl
     diffutils
-    docker-ce
-    docker-ce-cli
-    containerd.io
-    docker-buildx-plugin
-    docker-compose-plugin
+    docker.io
+    docker-compose-v2
     elfutils
     elpa-tuareg
     emacs-nox
